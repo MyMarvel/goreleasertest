bbc1d93a97f7370774cf1e58f856702b95ca3130
Nikita Petrov <mmmadaro@gmail.com>
2023-05-15 19:42:34 +0300

    v0.1.5

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	selfupdate "github.com/creativeprojects/go-selfupdate"
 )
 
-const version = "0.1.4"
+const version = "0.1.5"
 const repoName = "MyMarvel/goreleasertest"
 const delay = 15 * time.Second
 
