bdf6ad436ac57bb2ff336a40bf54581dab8ff5cb
Nikita Petrov <mmmadaro@gmail.com>
2023-05-15 18:35:07 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module goreleasertest
+
+go 1.19
+
+require (
+	code.gitea.io/sdk/gitea v0.15.1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.1 // indirect
+	github.com/creativeprojects/go-selfupdate v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-github/v30 v30.1.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
+	github.com/ulikunitz/xz v0.5.11 // indirect
+	github.com/xanzy/go-gitlab v0.83.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"time"
+
+	selfupdate "github.com/creativeprojects/go-selfupdate"
+)
+
+const version = "0.0.1"
+const repoName = "MyMarvel/goreleasertest"
+
+func main() {
+	for {
+		err := update(version)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(version)
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func update(version string) error {
+	latest, found, err := selfupdate.DetectLatest(context.Background(), selfupdate.ParseSlug(repoName))
+	if err != nil {
+		return fmt.Errorf("error occurred while detecting version: %w", err)
+	}
+	if !found {
+		return fmt.Errorf("latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH)
+	}
+
+	if latest.LessOrEqual(version) {
+		log.Printf("Current version (%s) is the latest", version)
+		return nil
+	}
+
+	exe, err := os.Executable()
+	if err != nil {
+		return errors.New("could not locate executable path")
+	}
+	if err := selfupdate.UpdateTo(context.Background(), latest.AssetURL, latest.AssetName, exe); err != nil {
+		return fmt.Errorf("error occurred while updating binary: %w", err)
+	}
+	log.Printf("Successfully updated to version %s", latest.Version())
+	return nil
+}
