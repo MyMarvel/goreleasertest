1b0b7f29e50362c09926103ee0a084dbb9899ba6
Nikita Petrov <mmmadaro@gmail.com>
2023-06-29 14:02:09 +0300

    v0.1.6

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 install:
 	go install github.com/goreleaser/goreleaser@latest
+tag:
+	echo "$(v)" > version.txt && git add -A && git commit -m "$(v)" && git tag -a $(v) -m "$(v)" && git push origin $(v)
 make:
 # Do not forget to import GITHUB_TOKEN, GITLAB_TOKEN or GITEA_TOKEN env variable
 # Then you should push a new tag to the repo, e.g. git tag -a v0.1.0 -m "First release" && git push origin v0.1.0
--- main.go
+++ main.go
@@ -12,25 +12,27 @@ import (
 	selfupdate "github.com/creativeprojects/go-selfupdate"
 )
 
-const version = "0.1.0"
+const version = "0.1.6"
 const repoName = "test/c2"
-const delay = 15 * time.Second
+const delay = 60 * time.Second
 
 func main() {
 	for {
+		fmt.Println("Before update")
 		err := update(version)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		fmt.Println(version)
 		time.Sleep(delay)
+		fmt.Println("After sleep")
 	}
 }
 
-func update(version string) error {
+func update2(version string) error {
 	source, err := selfupdate.NewGiteaSource(selfupdate.GiteaConfig{
 		BaseURL: "http://localhost:3000",
-		APIToken: "test", // manually created "Applications" token /user/settings/applications
+		APIToken: "45ea01a6c677552ea94d557a35a2fd4afd32d218", // manually created "Applications" token /user/settings/applications
 	})
 	if err != nil {
 		return err
@@ -64,5 +66,52 @@ func update(version string) error {
 		return fmt.Errorf("error occurred while updating binary: %w", err)
 	}
 	log.Printf("Successfully updated to version %s", latest.Version())
+	return nil
+}
+
+func update3(version string) error {
+	latest, found, err := selfupdate.DetectLatest(context.Background(), selfupdate.ParseSlug("MyMarvel/goreleasertest"))
+	if err != nil {
+		return fmt.Errorf("error occurred while detecting version: %w", err)
+	}
+	if !found {
+		return fmt.Errorf("latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH)
+	}
+
+	if latest.LessOrEqual(version) {
+		log.Printf("Current version (%s) is the latest", version)
+		return nil
+	}
+
+	exe, err := os.Executable()
+	if err != nil {
+		return errors.New("could not locate executable path")
+	}
+	if err := selfupdate.UpdateTo(context.Background(), latest.AssetURL, latest.AssetName, exe); err != nil {
+		return fmt.Errorf("error occurred while updating binary: %w", err)
+	}
+	log.Printf("Successfully updated to version %s", latest.Version())
+	return nil
+}
+
+func update(version string) error {
+	latest, found, err := selfupdate.DetectLatest(context.Background(), selfupdate.ParseSlug("MyMarvel/goreleasertest"))
+	if err != nil {
+		return fmt.Errorf("error occurred while detecting version: %w", err)
+	}
+	if !found {
+		return fmt.Errorf("latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH)
+	}
+
+	if latest.LessOrEqual(version) {
+		log.Printf("Current version (%s) is the latest", version)
+		return nil
+	}
+
+	re, err := selfupdate.UpdateSelf(context.Background(), version, selfupdate.ParseSlug("MyMarvel/goreleasertest"))
+	if err != nil {
+		return err
+	}
+	log.Printf("Successfully updated to version %s", re.Version())
 	return nil
 }
