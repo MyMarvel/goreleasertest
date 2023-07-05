d1262b4736c9e015def7f39792683b3460353961
Nikita Petrov <mmmadaro@gmail.com>
2023-07-05 12:29:46 +0300

    v0.2.25

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	selfupdate "github.com/creativeprojects/go-selfupdate"
 )
 
-const version = "0.2.24"
+const version = "0.2.25"
 const repoName = "test/c2"
 const delay = 60 * time.Second
 
